e67817a9968ad160c195813012777d827f4239b3
nonstdout <csalisbury@natilik.com>
2021-06-09 01:47:10 +0100

    static working

--- mod.js
+++ mod.js
@@ -1,14 +1,12 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.3.2/mod.ts";
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
 
-serve({
-    // You can serve a single file.
-    "/": serveStatic("public/index.html", { baseUrl: import.meta.url }),
-    "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
-    "/style.css": serveStatic("style.css", {
-        baseUrl: import.meta.url,
-        intervene: (request, response) => {
-            response.headers.set("content-type", "text/css; charset=utf-8");
-            return response;
-        },
-    }),
+const app = new Application();
+
+app.use(async (context) => {
+    await send(context, context.request.url.pathname, {
+        root: `${Deno.cwd()}/public`,
+        index: "index.html",
+    });
 });
+
+await app.listen({ port: 8000 });
