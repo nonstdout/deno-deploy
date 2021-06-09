b99e6df6e707d58fc919dbe1e66e89a249afc485
nonstdout <csalisbury@natilik.com>
2021-06-09 04:37:20 +0100

    again

--- mod.ts
+++ mod.ts
@@ -1,10 +1,11 @@
 import { Application, send } from "https://deno.land/x/oak/mod.ts";
+import * as path from "https://deno.land/std@0.97.0/path/mod.ts";
 
 const app = new Application();
 
 app.use(async (context) => {
     await send(context, context.request.url.pathname, {
-        root: `${Deno.cwd()}/public`,
+        root: path.fromFileUrl(new URL("./public", import.meta.url)),
         index: "index.html",
     });
 });
