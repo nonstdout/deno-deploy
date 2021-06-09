b3499bda19a333bde6460e5b5b5b4e16cfaff938
nonstdout <csalisbury@natilik.com>
2021-06-09 01:36:07 +0100

    try again

--- mod.js
+++ mod.js
@@ -1,6 +1,8 @@
 import { serve, serveStatic } from "https://deno.land/x/sift@0.3.2/mod.ts";
 
 serve({
+    // You can serve a single file.
+    "/": serveStatic("public/index.html", { baseUrl: import.meta.url }),
     "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
     "/style.css": serveStatic("style.css", {
         baseUrl: import.meta.url,
