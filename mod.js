0dfae84618c1b311d9d8254d0e418dd0796cb08d
nonstdout <csalisbury@natilik.com>
2021-06-09 03:06:09 +0100

    again

--- mod.js
+++ mod.js
@@ -1,12 +1,25 @@
-import { Application, send } from "https://deno.land/x/oak/mod.ts";
+import { serve, serveStatic } from "https://deno.land/x/sift@0.3.2/mod.ts";
 
-const app = new Application();
-
-app.use(async (context) => {
-    await send(context, context.request.url.pathname, {
-        root: `${Deno.cwd()}/public`,
-        index: "index.html",
-    });
+serve({
+    // You can serve a single file.
+    "/": serveStatic("public/index.html", { baseUrl: import.meta.url }),
+    // Or a directory of files.
+    "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
+    // Serve files hosted on the internet.
+    // The URL to the resouce would become https://yourbucket.aws.com/profile.png
+    // "/profile.png": serveStatic("profile.png", {
+    //     baseUrl: "https://yourbucket.aws.com",
+    // }),
+    // You can modify the fetched response before returning to the request
+    // by using the intervene option.
+    "/style.css": serveStatic("style.css", {
+        baseUrl: import.meta.url,
+        // The intervene function is called with the request as first argument and
+        // the fetched response as the second argument and it should return a
+        // response as a result.
+        intervene: (request, response) => {
+            response.headers.set("content-type", "text/css; charset=utf-8");
+            return response;
+        },
+    }),
 });
-
-await app.listen({ port: 8000 });
