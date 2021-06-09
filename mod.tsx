371e1a79d28139215dc5a98354bbcd92ab888561
nonstdout <csalisbury@natilik.com>
2021-06-09 23:47:45 +0100

    test

--- mod.tsx
+++ mod.tsx
@@ -9,11 +9,17 @@ import { h, jsx, serve, serveStatic } from "https://deno.land/x/sift@0.3.2/mod.t
 const App = () => (
   <div>
     <head >
+      <link rel="stylesheet" href="/style.css" />
+      <link rel="stylesheet" href="./style.css" />
       <link rel="stylesheet" href="style.css" />
     </head>
     <h1>Hello world!</h1>
     <img src="smiley.png" alt="" />
+    <img src="./smiley.png" alt="" />
+    <img src="/smiley.png" alt="" />
     <img src="PHlappstack.gif" alt="" />
+    <img src="./PHlappstack.gif" alt="" />
+    <img src="/PHlappstack.gif" alt="" />
   </div>
 );
 
