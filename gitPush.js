f69768e97aa370825867041dda878861917348e3
nonstdout <csalisbury@natilik.com>
2021-06-11 02:42:00 +0100

    'DENO COMMIT'

--- gitPush.js
+++ gitPush.js
@@ -1,23 +1,34 @@
 const userGithubRepo = "https://github.com/nonstdout/deno-deploy.git"
 
-const cmd = Deno.run({
-    cmd: ["git", "commit", "-am", "'DENO COMMIT'"],
-    stdout: "piped",
-    stderr: "piped",
+const commands = [
+    {
+        cmd: ["git", "commit", "-am", "'DENO COMMIT'"],
+        stdout: "piped",
+        stderr: "piped",
+    },
+    {
+        cmd: ["git", "push"],
+        stdout: "piped",
+        stderr: "piped",
+    },
+]
 
+commands.forEach(item => {
+    let cmd = Deno.run(item)
+
+
+    const { code } = cmd.status(); // (*1); wait here for child to finish
+
+    const rawOutput = cmd.output();
+    const rawError = cmd.stderrOutput();
+
+
+    if (code === 0) {
+        Deno.stdout.write(rawOutput);
+        console.log(`Commited ${userGithubRepo} sucessfully.`)
+    } else {
+        const errorString = new TextDecoder().decode(rawError);
+        console.log(errorString);
+    }
 })
-
-const { code } = await cmd.status(); // (*1); wait here for child to finish
-
-const rawOutput = await cmd.output();
-const rawError = await cmd.stderrOutput();
-
-
-if (code === 0) {
-    await Deno.stdout.write(rawOutput);
-    console.log(`Commited ${userGithubRepo} sucessfully.`)
-} else {
-    const errorString = new TextDecoder().decode(rawError);
-    console.log(errorString);
-}
 Deno.exit(code);
